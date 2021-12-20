9954cdf0050a74080af0982606f66e13417aa5bd
Admin <Admin@DESKTOP-GI61VKK>
2021-12-20 21:41:13 +0530

    h33i01222666

---
+++ src/moon/Scanner_User_Input.java
@@ -0,0 +1,37 @@
+package moon;
+
+import java.util.Scanner;
+
+public class Scanner_User_Input {
+
+	public static void main(String[] args) {
+//Scanner sc=new Scanner(System.in);
+//System.out.println("num1");
+//    int num1 = sc.nextInt();
+//    System.out.println("num2");
+//    int num2 = sc.nextInt();
+//    System.out.println("sum");
+//    int sum = num1+num2;
+//    System.out.println("sum is "+sum);
+
+//		Scanner SC=new Scanner(System.in);
+//		System.out.println("num1");
+//		int num1=SC.nextInt();
+//		System.out.println("num2");
+//		int num2=SC.nextInt();
+//        int sum=num1+num2;
+//		System.out.println("sum is " + sum);
+
+		Scanner po=new Scanner(System.in) ;
+		System.out.println("num1");
+		int num1 = po.nextInt();
+		System.out.println("num2");
+		int num2 = po.nextInt();
+		
+		int sum = num1+num2;
+		
+	}	
+		
+		
+
+}
